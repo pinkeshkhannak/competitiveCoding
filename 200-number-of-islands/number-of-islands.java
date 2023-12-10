9662ba1adec2a2775f13cacb9776da61c4db596d
pinkeshkhannak <52904690+pinkeshkhannak@users.noreply.github.com>
2023-12-10 19:14:59 +0530

    Time: 3 ms (72.90%) | Memory: 47.9 MB (35.87%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -2,33 +2,27 @@ class Solution {
     public void dfs(int i, int j, boolean vis[][], char grid[][]) {
         int n = grid.length;
         int m = grid[0].length;
-
-        if (i < 0 || j < 0 || i >= n || j >= m || vis[i][j] || grid[i][j] == '0') {
+        if(i<0||j<0||i>=n||j>=m||grid[i][j] == '0'||vis[i][j]) {
             return;
-        }
-
+        } 
         vis[i][j] = true;
-
-        // Explore adjacent cells (horizontal and vertical movements)
-        dfs(i + 1, j, vis, grid); // Down
-        dfs(i - 1, j, vis, grid); // Up
-        dfs(i, j + 1, vis, grid); // Right
-        dfs(i, j - 1, vis, grid); // Left
+        dfs(i+1,j,vis,grid);
+        dfs(i-1,j,vis,grid);
+        dfs(i,j+1,vis,grid);
+        dfs(i,j-1,vis,grid);
+        
     }
-
     public int numIslands(char[][] grid) {
         int n = grid.length;
         if (n == 0) return 0;
         int m = grid[0].length;
-
         boolean vis[][] = new boolean[n][m];
         int count = 0;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (!vis[i][j] && grid[i][j] == '1') {
+        for(int i = 0;i<n;i++) {
+            for(int j =0;j<m;j++) {
+                if(!vis[i][j] && grid[i][j] == '1') {
                     count++;
-                    dfs(i, j, vis, grid);
+                    dfs(i,j,vis,grid);
                 }
             }
         }
