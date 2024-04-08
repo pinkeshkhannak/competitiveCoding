278462fc49e7643b91bcaeb37239981c157e5b12
pinkeshkhannak <52904690+pinkeshkhannak@users.noreply.github.com>
2024-04-09 00:10:02 +0530

    Time: 2 ms (18.02%) | Memory: 44.5 MB (94.83%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -1,23 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         
-        List<List<Integer>> list = new ArrayList<>();
-        if(root == null) {
-            return list;
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root == null) return ans;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()) {
+           int size = queue.size();
+           List<Integer> ds = new ArrayList<>();
+           for(int i =0;i<size;i++) {
+               TreeNode temp = queue.poll();
+               ds.add(temp.val);
+               if(temp.left != null) {
+                   queue.add(temp.left);
+               }
+               if(temp.right != null) {
+                   queue.add(temp.right);
+               }
+           }
+           ans.add(new ArrayList<>(ds));
         }
-        Queue<TreeNode> q = new LinkedList<TreeNode>();
-        q.offer(root);
-        while(!q.isEmpty()) {
-            List<Integer> ans = new ArrayList<>();
-            int size = q.size();
-            for(int i = 0;i<size;i++) {
-                TreeNode node = q.poll();
-                ans.add(node.val);
-                if(node.left != null) q.add(node.left);
-                if(node.right != null) q.add(node.right);
-            }
-            list.add(ans);  
-        }
-        return list;
+        return ans;
     }
+    
 }
